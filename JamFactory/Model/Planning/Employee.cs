d1fdeea86a506546cd4eb9b5032007f00f1ca373
Jeppeu <Jeppeuldall@gmail.com>
2014-03-24 12:56:54 +0100

    New class
    Tilføjet employee class!

---
+++ JamFactory/Model/Planning/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Planning
+{
+    class Employee
+    {
+        private int id { get; set; }
+        private string Name { get; set; }
+        private decimal hours { get; set; }
+        private Pay pay { get; set; }
+
+    }
+}
