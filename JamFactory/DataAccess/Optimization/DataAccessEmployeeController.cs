d92a56a024d596d0e1bc423b84e69a48679ae874
atkonline <atkonline@gmail.com>
2014-03-27 13:50:50 +0100

    yolo

--- JamFactory/DataAccess/Optimization/DataAccessEmployeeController.cs
+++ JamFactory/DataAccess/Optimization/DataAccessEmployeeController.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data.Sql;
 
-namespace DataAccess
+namespace DataAccess.Optimization
 {
-    abstract class DataAccessController
+   
+    class DataAccessEmployeeController
     {
         public const string CONNECTION_STRING = "Data Source=ealdb1.eal.local;Initial Catalog=EJL15_DB;Persist Security Info=True;User ID=ejl15_usr;Password=Baz1nga15";
+      
+        DataAccessEmployeeController() { } 
 
-       
     }
 }
