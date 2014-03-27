4adb5cfdae7c6a5615c4892338c5c3ad06224f5b
alfeko <ferrari7a@gmail.com>
2014-03-27 12:49:57 +0100

    Added model class and dataAccess entities

---
+++ JamFactory/Model/Products/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Interfaces;
+
+namespace Model.Products
+{
+    public class Product : IProduct
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int ContainerId { get; set; }
+        public int ProductTypeId { get; set; }
+        public int QualityId { get; set; }
+        public int RecipeId { get; set; }
+    }
+}
