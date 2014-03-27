8ba5aa35c011aede8edab012d18039fc18f42dc2
alfeko <ferrari7a@gmail.com>
2014-03-27 09:03:00 +0100

    Added ConnectionString
    (VPN Required for connection)

---
+++ JamFactory/DataAccess/DataAccessController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    abstract class DataAccessController
+    {
+        public static string ConnectionString = "Data Source=ealdb1.eal.local;Initial Catalog=EJL15_DB;Persist Security Info=True;User ID=ejl15_usr;Password=Baz1nga15";
+    }
+}
