2515bd9b31da5eadce55e0c5f619aa3875763666
Jeppeu <Jeppeuldall@gmail.com>
2014-03-27 11:21:30 +0100

    Tesst
    Interfaces tilføjet

---
+++ JamFactory/Common/Interfaces/IPay.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Interfaces
+{
+    interface IPay
+    {
+        decimal totalHours;
+        DateTime startDate;
+        DateTime endTime;
+        decimal eveneningHours;
+        decimal weekendHours;
+        decimal nightHours;
+        decimal totalPay;
+
+    }
+}
