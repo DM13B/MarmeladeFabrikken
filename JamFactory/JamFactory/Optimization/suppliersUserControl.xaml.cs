4f9527ca7ce32d750dd36c64870ef8ae803c3a87
rfirevtre <peter@akim.dk>
2014-03-27 13:19:11 +0100

    UI tæt på done
    klar til demo

--- JamFactory/JamFactory/Optimization/suppliersUserControl.xaml.cs
+++ JamFactory/JamFactory/Optimization/suppliersUserControl.xaml.cs
@@ -13,17 +13,16 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace JamFactory
+namespace JamFactory.Optimization
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Suppliers.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class SuppliersUserControl : UserControl
     {
-        public MainWindow()
+        public SuppliersUserControl()
         {
             InitializeComponent();
-            optimizationUserControl.Content = new Optimization.OptimizationUserControl();
         }
     }
 }
---
+++ JamFactory/Model/Optimization/Supplier.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Optimization
+{
+    class Supplier
+    {
+        public string rawMaterial { get; set; }
+        public string amountKg { get; set; }
+        public string priceKg { get; set; }
+        public string supplierName { get; set; }
+    }
+}
