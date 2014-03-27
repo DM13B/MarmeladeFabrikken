e1134d67aa5642ba08355ad8a0d55efee59bb44e
alfeko <ferrari7a@gmail.com>
2014-03-27 11:49:54 +0100

    Gruppe2Interfaces
    Container, Product, ProductType, Quality and Recipe interfaces

---
+++ JamFactory/Common/Interfaces/IContainer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Interfaces
+{
+    public interface IContainer
+    {
+        int Id { get; set; }
+        int Quantity { get; set; }
+    }
+}
