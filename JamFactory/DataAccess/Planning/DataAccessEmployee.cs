1e36d4885b239f1a5c08c6635fa61c339b15b02e
atkonline <atkonline@gmail.com>
2014-04-02 12:22:39 +0200

    dataaccessupdate

--- JamFactory/DataAccess/Planning/DataAccessEmployee.cs
+++ JamFactory/DataAccess/Planning/DataAccessEmployee.cs
@@ -7,7 +7,7 @@ using Model.Planning;
 using Common.Interfaces;
 using System.Data.Sql;
 using System.Data.SqlClient;
-using System.Data.SqlDbType;
+
 
 
 namespace DataAccess.Planning
@@ -19,21 +19,30 @@ namespace DataAccess.Planning
         SqlConnection conn = new SqlConnection(connString);
         
         public void CreateEmployee(Employee employee)
-        { conn.Open();
-        SqlCommand cmd = new SqlCommand("EmployeeCreate", conn); // insert command
-        cmd.CommandType = System.Data.CommandType.StoredProcedure;
-        cmd.Parameters.Add("@theFirstName", System.Data.SqlDbType.VarChar).Value = employee.FirstName;
-        cmd.Parameters.Add("@theLastName", System.Data.SqlDbType.VarChar).Value = employee.LastName;
-        cmd.Parameters.Add("@theHoursPrWeek", System.Data.SqlDbType.Float).Value = (float)employee.HoursPrWeek;
-        cmd.Parameters.Add("@theHourlyRate", System.Data.SqlDbType.Float).Value = (float)employee.HourlyRate;
-
-        conn.Open();
-        cmd.ExecuteNonQuery();     //Boom Shakalakalaka
-
-        //@theFirstName varchar(100),
-        //@theLastName  varchar(100),
-        //@theHoursPrWeek float,
-        //@theHourlyRate float
+        {           conn.Open();
+              try
+              {
+                       SqlCommand cmd = new SqlCommand("EmployeeCreate", conn); // insert command
+                              cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                              cmd.Parameters.Add("@theFirstName", System.Data.SqlDbType.VarChar).Value = employee.FirstName;
+                              cmd.Parameters.Add("@theLastName", System.Data.SqlDbType.VarChar).Value = employee.LastName;
+                              cmd.Parameters.Add("@theHoursPrWeek", System.Data.SqlDbType.Float).Value = (float)employee.HoursPrWeek;
+                              cmd.Parameters.Add("@theHourlyRate", System.Data.SqlDbType.Float).Value = (float)employee.HourlyRate;
+                      
+                              conn.Open();
+                              cmd.ExecuteNonQuery();     //Boom Shakalakalaka
+                      
+                              //@theFirstName varchar(100),
+                              //@theLastName  varchar(100),
+                              //@theHoursPrWeek float,
+                              //@theHourlyRate float
+                              }
+                              catch (Exception)
+              {
+                  
+                  throw;
+              }
+       
         
         }
     }
