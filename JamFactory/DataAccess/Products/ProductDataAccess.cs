4e61a6b116b74af8b638f6375003721e91ed6c89
alfeko <ferrari7a@gmail.com>
2014-04-02 09:46:06 +0200

    More productController stuff + added automapper

--- JamFactory/DataAccess/Products/ProductDataAccess.cs
+++ JamFactory/DataAccess/Products/ProductDataAccess.cs
@@ -26,8 +26,8 @@ namespace DataAccess.Products
 
             using (var cmd = new SqlCommand("ProductCreate", _conn))
             {
-                cmd.Parameters.Add("@ID", SqlDbType.Int).Value = p.Id;
                 cmd.Parameters.Add("@Name", SqlDbType.NVarChar).Value = p.Name;
+                cmd.Parameters.Add("@Description", SqlDbType.NVarChar).Value = p.Description;
                 cmd.Parameters.Add("@ContainerID", SqlDbType.Int).Value = DBNull.Value;
                 cmd.Parameters.Add("@ProductTypeID", SqlDbType.Int).Value = DBNull.Value;
                 cmd.Parameters.Add("@QualityID", SqlDbType.Int).Value = DBNull.Value;
@@ -39,34 +39,95 @@ namespace DataAccess.Products
             _conn.Close();
 
         }
+        
         /// <summary>
         /// Get product by ID
         /// </summary>
         /// <param name="id">This is the product ID that will be searched on.</param>
-        public void GetProductById(int id)
+        public IProduct GetProductById(int id)
         {
-            ProductEntity p = new ProductEntity();
+            ProductEntity p = null;
 
             using (var sqlC = new SqlCommand("ProductGetByID", _conn))
             {
                 sqlC.Parameters.Add("@ID", SqlDbType.Int).Value = id;
-                _conn.Open();
-                sqlC.ExecuteNonQuery();
+                SqlDataReader reader = sqlC.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    p = new ProductEntity
+                    {
+                        Id = reader.GetInt32(0),
+                        Name = reader.GetString(1),
+                        Description = reader.GetString(2),
+                        ContainerId = reader.GetInt32(3),
+                        ProductTypeId = reader.GetInt32(4),
+                        QualityId = reader.GetInt32(5),
+                        RecipeId = reader.GetInt32(6)
+                    };
+                }
             }
             _conn.Close();
-            //Still needs a return
+
+            return p as IProduct;
         }
 
         /// <summary>
         /// Get all products.. nuff said
         /// </summary>
-        public void GetAllProducts()
+        public List<ProductEntity> GetAllProducts()
         {
+            List<ProductEntity> products = new List<ProductEntity>();
             using(var sqlC = new SqlCommand("ProductGetAll", _conn))
             {
-               
+                SqlDataReader reader = sqlC.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    ProductEntity product = new ProductEntity
+                    {
+                        Id = reader.GetInt32(0),
+                        Name = reader.GetString(1),
+                        Description = reader.GetString(2),
+                        ContainerId = reader.GetInt32(3),
+                        ProductTypeId = reader.GetInt32(4),
+                        QualityId = reader.GetInt32(5),
+                        RecipeId = reader.GetInt32(6)
+                    };
+
+                    products.Add(product);
+                }
+                _conn.Close();
             }
+            return products;
         }
 
+        public void UpdateProduct(IProduct product)
+        {
+            using (SqlCommand sqlC = new SqlCommand("ProductUpdate", _conn))
+            {
+                sqlC.Parameters.Add("@ID", SqlDbType.Int).Value = product.Id;
+                sqlC.Parameters.Add("@Name", SqlDbType.NVarChar).Value = product.Name;
+                sqlC.Parameters.Add("@Description", SqlDbType.Int).Value = product.Description;
+                sqlC.Parameters.Add("@ContainerID", SqlDbType.Int).Value = product.ContainerId;
+                sqlC.Parameters.Add("@ProductTypeID", SqlDbType.Int).Value = product.ProductTypeId;
+                sqlC.Parameters.Add("@QualityID", SqlDbType.Int).Value = product.QualityId;
+                sqlC.Parameters.Add("@RecipeID", SqlDbType.Int).Value = product.RecipeId;
+                sqlC.ExecuteNonQuery();
+            }
+        }
+        /// <summary>
+        /// Delete product
+        /// </summary>
+        /// <param name="product">Insert the product that you want to delete</param>
+        /// <returns></returns>
+        public void DeleteProduct(IProduct product)
+        {
+            using (SqlCommand sqlC = new SqlCommand("ProductDelete", _conn))
+            {
+                sqlC.Parameters.Add("@ID", SqlDbType.Int).Value = product.Id;
+                sqlC.ExecuteNonQuery();
+            }
+        }
     }
 }
