61e05a311a7c429004e9ade834b3bd6038ccb84f
rfirevtre <peter@akim.dk>
2014-04-02 12:03:19 +0200

    added receivedGoodsEntity og OptimizationDataAccess

---
+++ JamFactory/DataAccess/Optimization/OptimizationDataAccess.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Optimization
+{
+    public class OptimizationDataAccess : DataAccessController
+    {
+        private SqlConnection _conn;
+        public void CreateSqlConnection()
+        {
+            _conn = new SqlConnection(ConnectionString);
+        }
+
+        public void GetAllReceivedGoods()
+        {
+
+
+        }
+    }
+}
---
+++ JamFactory/DataAccess/Products/Entities/ReceivedGoodsEntity.cs
@@ -0,0 +1,18 @@
+﻿using Common.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Products.Entities
+{
+    class ReceivedGoodsEntity
+    {
+        public IRawGoods RawGoods { get; set; }
+        public double Amount { get; set; }
+        public decimal Price { get; set; }
+        public DateTime Received { get; set; }
+        public string Supplier { get; set; }
+    }
+}
