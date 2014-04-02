455b9fd8caf1398f7b9e4cef16c721e820b37b6a
Simon Madsen <simon@smadsen.com>
2014-04-02 09:47:57 +0200

    Added Container DataAccess

---
+++ JamFactory/DataAccess/Products/ContainerDataAccess.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using Common.Interfaces;
+using DataAccess.Products.Entities;
+using System.Data;
+namespace DataAccess.Products
+{
+    public class ContainerDataAccess : DataAccessController
+    {
+        private SqlConnection _conn;
+        public ContainerDataAccess()
+        {
+            _conn = new SqlConnection(ConnectionString);
+        }
+
+
+        /// <summary>
+        /// Create Container
+        /// </summary>
+        /// <param name="container">Insert the container to be created in the database</param>
+        public void CreateContainer(IContainer container)
+        {
+            ContainerEntity c = (ContainerEntity)container;
+
+            using (var cmd = new SqlCommand("ContainerCreate", _conn))
+            {
+                cmd.Parameters.Add("@Quantity", SqlDbType.Int).Value = c.Quantity;
+                _conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+            _conn.Close();
+
+        }
+
+
+
+    }
+}
---
+++ JamFactory/UnitTests/Products/DataAccess/ContainerDataAccessTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DataAccess.Products;
+using Common.Interfaces;
+using Model.Products;
+namespace UnitTests.Products.DataAccess
+{
+    [TestClass]
+    public class ContainerDataAccessTest
+    {
+        [TestMethod]
+        public void TestCreate()
+        {
+            IContainer p = new Container
+            {
+                 Quantity = 22
+            };
+            ContainerDataAccess da = new ContainerDataAccess();
+            da.CreateContainer(p);
+            if (2 == 3)
+            {
+                Assert.Fail();
+            }
+        }
+    }
+}
