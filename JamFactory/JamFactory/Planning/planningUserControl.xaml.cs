8314c5a1ad5d05b4b3805911a600b8ef0df42365
atkonline <atkonline@gmail.com>
2014-04-07 09:52:22 +0200

    updatthatstuff

--- JamFactory/JamFactory/Planning/planningUserControl.xaml.cs
+++ JamFactory/JamFactory/Planning/planningUserControl.xaml.cs
@@ -13,19 +13,18 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace JamFactory
+namespace JamFactory.Planning
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for UserControl1.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class planningUserControl : UserControl
     {
-        public MainWindow()
+        public planningUserControl()
         {
             InitializeComponent();
-            optimizationUserControl.Content = new Optimization.OptimizationUserControl();
-            productsUserControl.Content = new Products.productsUserControl();
-            planningUserControl.Content = new Planning.planningUserControl();
+            
+           
         }
     }
 }
